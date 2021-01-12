5da32a2d6ab42ad8906171f074c8dcb3c393d1c8
crusher083 <61702053+crusher083@users.noreply.github.com>
2021-01-12 19:17:54 +0100

    minor updates for readability

--- src/data/preprocessing.py
+++ src/data/preprocessing.py
@@ -1,19 +1,31 @@
 import os
 from typing import List
 from numpy import ndarray, newaxis, zeros
-import nrrd
+from nrrd import read
 from pydicom import read_file
 
 
 def dicom_list(data_dir: str) -> List[str]:
     '''Locate all .dcm files in directory and return filenames as list'''
     dicoms = []
-    for dirs, subdirs, filenames in os.walk(data_dir):
 
+    for dirs, subdirs, filenames in os.walk(data_dir):
         for filename in filenames:
             if ".dcm" in filename.lower():  # check whether the file's DICOM
                 dicoms.append(os.path.join(data_dir, filename))
     return sorted(dicoms)
+
+
+def nrrd_list(data_dir: str, region: str) -> List[str]:
+    '''Locate referenced .nrrd files in directory'''
+    nrrd_list = []
+
+    for dirs, subdirs, filenames in os.walk(data_dir):
+        for filename in filenames:
+            if region.lower() in filename.lower():
+                nrrd_list.append(os.path.join(data_dir, filename))
+
+    return sorted(nrrd_list)
 
 
 def list_as_arr(filenames_list: List[str]) -> ndarray:
@@ -22,7 +34,6 @@ def list_as_arr(filenames_list: List[str]) -> ndarray:
         ref_dcm = read_file(filenames_list[0])
         # Load dimensions based on the number of rows & columns
         dims = (len(filenames_list), int(ref_dcm.Rows), int(ref_dcm.Columns))
-
         array = zeros(dims, dtype=ref_dcm.pixel_array.dtype)
 
         for filename in filenames_list:
@@ -30,16 +41,17 @@ def list_as_arr(filenames_list: List[str]) -> ndarray:
             ds = read_file(filename)
             # store the raw image data
             array[filenames_list.index(filename), :, :] = ds.pixel_array
-    elif filenames_list[0].endswith('.nrrd'):
-        ref_nrrd, header = nrrd.read(filenames_list[0])
 
+    elif filenames_list[0].endswith('.nrrd'):
+        ref_nrrd, header = read(filenames_list[0])
         # Load dimensions based on the number of rows & columns
         dims = (len(filenames_list), ref_nrrd.shape[0], ref_nrrd.shape[1])
         array = zeros(dims)
 
         for filename in filenames_list:
-            data, header = nrrd.read(filename)
+            data, header = read(filename)
             array[filenames_list.index(filename), :, :] = data.T
+
     return array[:, :, :, newaxis]
 
 
@@ -51,23 +63,13 @@ def load_dicom(data_dir: str) -> ndarray:
     return dicom_array
 
 
-def nrrd_list(data_dir: str, region: str) -> List[str]:
-    '''Locate referenced .nrrd files in directory'''
-    nrrd_list = []
-    for dirs, subdirs, filenames in os.walk(data_dir):
-
-        for filename in filenames:
-            if region in filename.lower():  # check whether the file's DICOM
-                nrrd_list.append(os.path.join(data_dir, filename))
-
-    return sorted(nrrd_list)
-
 def load_nrrd(data_dir: str, region: str) -> ndarray:
     '''Load all .nrrd masks for specified region'''
     list_from_dir = nrrd_list(data_dir, region)
     nrrd_array = list_as_arr(list_from_dir)
     print(f'{len(list_from_dir)} .nrrd masks from {region} loaded.')
     return nrrd_array
+
 
 if __name__ == '__main__':
     load_dicom(data_dir)
